f2a65414936464994800469d9dac7cdad48be563
Oni_Dev <Oni_Dev@DESKTOP>
2026-07-25 22:53:15 +0200

    quitados warnings 2

--- app/src/main/java/com/example/ruts/presentation/screens/RouteEditorScreen.kt
+++ app/src/main/java/com/example/ruts/presentation/screens/RouteEditorScreen.kt
@@ -51,7 +51,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import com.example.ruts.data.RouteRepository
